6e35b56e2552817d20d419e826413682b35ffabe
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2023-05-18 19:34:25 +0100

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import starIcon from './assets/images/icon-star.svg'
+import thankYouIllustration from './assets/images/illustration-thank-you.svg'
+
+function App() {
+  return <div className='min-h-screen grid place-items-center'>Hello World</div>
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: 'hsl(25, 97%, 53%)',
+        light_grey: 'hsl(217, 12%, 63%)',
+        medium_grey: 'hsl(216, 12%, 54%)',
+        dark_blue: 'hsl(213, 19%, 18%)',
+        very_dark_blue: 'hsl(216, 12%, 8%)',
+      },
+      fontSize: {
+        paragraph: '15px',
+      },
+      fontFamily: {
+        overpass: ['Overpass', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
